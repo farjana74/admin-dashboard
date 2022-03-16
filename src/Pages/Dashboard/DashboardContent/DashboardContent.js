bfeb0f1f9fd1a1485fe2e99c0c94e088e7cd9893
farjana74 <csefarjana74@gmail.com>
2022-03-16 14:50:10 +0600

    added

---
+++ src/Pages/Dashboard/DashboardContent/DashboardContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardContent = () => {
+    return (
+        <div>
+            <h3>jfdkjkjb</h3>
+        </div>
+    );
+};
+
+export default DashboardContent;
